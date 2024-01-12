b3832ce7ec4a93729380af884906a95f230d15e4
FlavioRoman <romanflavio2@gmail.com>
2024-01-11 23:52:13 -0300

    update

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -24,7 +24,7 @@ export default function Main() {
       <Products />
       <Dealers />
       <Testimonials />
-      {/* <Authentication /> */}
+      <Authentication />
       <Footer />
       {show && <TopButton />}
     </ParallaxProvider>
